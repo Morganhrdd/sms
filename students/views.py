286f548b712b2f1d3151dee77322786837143314
Ashutosh <ashutosh dot barmukh at gmail dot com@localhost>
2009-03-18 22:04:25 +0530

    Updated grade evaluation code.

--- students/views.py
+++ students/views.py
@@ -16,6 +16,7 @@ from reportlab.lib.units import inch
 from reportlab.lib import colors
 import os
 from django.contrib.auth.decorators import login_required
+from PIL import Image
 
 PAGE_HEIGHT=defaultPageSize[1]; PAGE_WIDTH=defaultPageSize[0]
 styles = getSampleStyleSheet()
@@ -23,6 +24,52 @@ styles = getSampleStyleSheet()
 Title = "Jnana Prabodhini Prashala"
 pageinfo = "School Report"
 
+MONTH_CHOICES = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
+
+GRADE_CHOICES = {
+    'O': 'Outstanding',
+    'E': 'Excellent',
+    'G': 'Good',
+    'S': 'Satisfactory',
+    'N': 'Needs Improvement',
+    'U': 'Unsatisfactory',
+    'A': 'No Entry for this category'
+}
+
+GRADE_NUM = {
+    'O': 6,
+    'E': 5,
+    'G': 4,
+    'S': 3,
+    'N': 2,
+    'U': 1,
+    'A':0
+}
+
+GRADE_CHOICE_NUM = ['No Entry for this category','Unsatisfactory','Needs Improvement','Satisfactory','Good','Excellent','Outstanding']
+
+PROJECT_TYPE_CHOICES = {
+    'CC': 'Collection, Classification',
+    'MM': 'Model Making',
+    'IS': 'Investivation by Survey',
+    'I': 'Investigation',
+    'CP': 'Creative production',
+    'AC': 'Appreciation-criticism',
+    'O': 'Open ended exploration',
+}
 def report(request):
     if request.POST:
         keys = request.POST.keys()
@@ -38,59 +85,92 @@ def report(request):
         attendances = StudentAttendance.objects.filter(StudentYearlyInformation = student_yearly_info)
         attendance_data = []
         for attendance in attendances:
-            attendance_data.append({'Month':attendance.AttendanceMaster.Month , 'Attendance':attendance.ActualAttendance , 'Working_days':attendance.AttendanceMaster.WorkingDays})
+            attendance_data.append({'Month':MONTH_CHOICES[attendance.AttendanceMaster.Month] , 'Attendance':attendance.ActualAttendance , 'Working_days':attendance.AttendanceMaster.WorkingDays})
      
         marks = StudentTestMarks.objects.filter(StudentYearlyInformation=student_yearly_info)
         mark_data = []
         for mark in marks:
             mark_data.append({'MarksObtained':mark.MarksObtained , 'MaximumMarks':mark.TestMapping.MaximumMarks ,'Subject_Name':mark.TestMapping.SubjectMaster.Name,'test_type':mark.TestMapping.TestType})
+
+        im=Image.open('media/'+student_yearly_info.Photo)
+        photo = []
+        photo.append(im.show())
         
         co_curricular = CoCurricular.objects.filter(StudentYearlyInformation = student_yearly_info)
         co_curricular_data = []
+        cumulative_cocur_grade_sum=0
+        cumulative_cocur_grade=0
         for co_cur_acts in co_curricular:
+            cumulative_cocur_grade_sum=cumulative_cocur_grade_sum + GRADE_NUM[co_cur_acts.Grade]
             co_curricular_data.append({'Activity':co_cur_acts.Activity , 'Objectives':co_cur_acts.Objectives ,'Date':co_cur_acts.Date ,'Guide':co_cur_acts.Guide ,'Grade':co_cur_acts.Grade ,'PublicComment':co_cur_acts.PublicComment})
-
-
+        cumulative_cocur_grade=GRADE_CHOICE_NUM[int(round(cumulative_cocur_grade_sum/len(co_curricular)))]        
+        print cumulative_cocur_grade
+        
         abhivyakti_vikas = AbhivyaktiVikas.objects.filter(StudentYearlyInformation = student_yearly_info)
         abhivyakti_vikas_data = []
+        cumulative_abhi_grade_sum=0
+        cumulative_abhi_grade=0
         for abhi_row in abhivyakti_vikas:
             print abhi_row.MediumOfExpression
+            abhi_grade_row_sum=int(GRADE_NUM[abhi_row.Participation])+int(GRADE_NUM[abhi_row.ReadinessToLearn])+int(GRADE_NUM[abhi_row.ContinuityInWork])+int(GRADE_NUM[abhi_row.SkillDevelopment])+int(GRADE_NUM[abhi_row.Creativity])
+            cumulative_abhi_grade_sum=cumulative_abhi_grade_sum+(int(abhi_grade_row_sum/5))
             abhivyakti_vikas_data.append({'MediumOfExpression':abhi_row.MediumOfExpression ,
                                          'Teacher':abhi_row.Teacher ,
-                                         'Participation':abhi_row.Participation ,
-                                         'ReadinessToLearn':abhi_row.ReadinessToLearn ,
-                                         'ContinuityInWork':abhi_row.ContinuityInWork ,
-                                         'SkillDevelopment':abhi_row.SkillDevelopment,
-                                         'Creativity':abhi_row.Creativity})
+                                         'Participation':GRADE_CHOICES[abhi_row.Participation] ,
+                                         'ReadinessToLearn':GRADE_CHOICES[abhi_row.ReadinessToLearn] ,
+                                         'ContinuityInWork':GRADE_CHOICES[abhi_row.ContinuityInWork] ,
+                                         'SkillDevelopment':GRADE_CHOICES[abhi_row.SkillDevelopment],
+                                         'Creativity':GRADE_CHOICES[abhi_row.Creativity]})
+        cumulative_abhi_grade=GRADE_CHOICE_NUM[int(round(cumulative_abhi_grade_sum/len(abhivyakti_vikas)))]
+        print cumulative_abhi_grade
 
 
         projects = Project.objects.filter(StudentYearlyInformation = student_yearly_info)
         project_data = []
+        cumulative_project_grade_sum=0
+        cumulative_project_grade=0
         for proj_row in projects:
             print proj_row.Title
+            proj_grade_row_sum=int(GRADE_NUM[proj_row.ProblemSelection])+int(GRADE_NUM[proj_row.Review])+int(GRADE_NUM[proj_row.Planning])+int(GRADE_NUM[proj_row.Documentation])+int(GRADE_NUM[proj_row.Communication])
+            cumulative_project_grade_sum=cumulative_project_grade_sum+(int(proj_grade_row_sum/5))
             project_data.append({'Title':proj_row.Title ,
-                                         'Type':proj_row.Type ,
+                                         'Type':PROJECT_TYPE_CHOICES[proj_row.Type] ,
                                          'Subject':proj_row.Subject ,
-                                         'ProblemSelection':proj_row.ProblemSelection ,
-                                         'Review':proj_row.Review ,
-                                         'Planning':proj_row.Planning,
-                                         'Documentation':proj_row.Documentation,
-                                         'Communication':proj_row.Communication})
+                                         'ProblemSelection':GRADE_CHOICES[proj_row.ProblemSelection] ,
+                                         'Review':GRADE_CHOICES[proj_row.Review] ,
+                                         'Planning':GRADE_CHOICES[proj_row.Planning],
+                                         'Documentation':GRADE_CHOICES[proj_row.Documentation],
+                                         'Communication':GRADE_CHOICES[proj_row.Communication]})
+        cumulative_project_grade=GRADE_CHOICE_NUM[int(round(cumulative_project_grade_sum/len(projects)))]
+        print cumulative_project_grade
+
 
         elocution = Elocution.objects.filter(StudentYearlyInformation = student_yearly_info)
         elocution_data = []
+        cumulative_elocution_grade_sum=0
+        cumulative_elocution_grade=0
         for elo_row in elocution:
+            print 'ELOC'
             print elo_row.Title
+            print elo_row.Memory
+            print elo_row.Content
+            elocution_grade_row_sum=int(GRADE_NUM[elo_row.Memory])+int(GRADE_NUM[elo_row.Content])+int(GRADE_NUM[elo_row.Understanding])+int(GRADE_NUM[elo_row.Skill])+int(GRADE_NUM[elo_row.Presentation])
+            cumulative_elocution_grade_sum=cumulative_elocution_grade_sum+(int(elocution_grade_row_sum/5))
             elocution_data.append({'Title':elo_row.Title ,
-                                         'Memory':elo_row.Memory ,
-                                         'Content':elo_row.Content ,
-                                         'Understanding':elo_row.Understanding ,
-                                         'Skill':elo_row.Skill ,
-                                         'Presentation':elo_row.Presentation})
+                                         'Memory':GRADE_CHOICES[elo_row.Memory] ,
+                                         'Content':GRADE_CHOICES[elo_row.Content] ,
+                                         'Understanding':GRADE_CHOICES[elo_row.Understanding] ,
+                                         'Skill':GRADE_CHOICES[elo_row.Skill] ,
+                                         'Presentation':GRADE_CHOICES[elo_row.Presentation]})
+        cumulative_elocution_grade=GRADE_CHOICE_NUM[int(round(cumulative_elocution_grade_sum/len(elocution)))]
+        print cumulative_elocution_grade
 
         physical_fit_info = PhysicalFitnessInfo.objects.filter(StudentYearlyInformation = student_yearly_info)
         physical_fit_info_data = []
+        cumulative_physical_grade_sum=0
+        cumulative_physical_grade=0
         for ph_data in physical_fit_info:
+            cumulative_physical_grade_sum=cumulative_physical_grade_sum + GRADE_NUM[ph_data.Grade]
             physical_fit_info_data.append({'Pathak':ph_data.Pathak ,
                                            'Pratod':ph_data.Pratod ,
                                            'Margadarshak':ph_data.Margadarshak ,
@@ -111,27 +191,41 @@ def report(request):
                                            'Balancing':ph_data.Balancing ,
                                            'Grade':ph_data.Grade ,
                                            'PublicComment':ph_data.PublicComment})
+        cumulative_physical_grade=GRADE_CHOICE_NUM[int(round(cumulative_physical_grade_sum/len(physical_fit_info)))]        
+        print cumulative_physical_grade
 
         social_activities = SocialActivity.objects.filter(StudentYearlyInformation = student_yearly_info)
         social_activity_data = []
+        cumulative_social_grade_sum=0
+        cumulative_social_grade=0
         for soc_act_data in social_activities:
-            print soc_act_data.Activity
+            cumulative_social_grade_sum=cumulative_social_grade_sum + GRADE_NUM[soc_act_data.Grade]
             social_activity_data.append({'Activity':soc_act_data.Activity ,
                                          'Objectives':soc_act_data.Objectives ,
                                          'Date':soc_act_data.Date ,
                                          'Organizer':soc_act_data.Organizer ,
                                          'Grade':soc_act_data.Grade ,
                                          'PublicComment':soc_act_data.PublicComment})
+        cumulative_social_grade=GRADE_CHOICE_NUM[int(round(cumulative_social_grade_sum/len(social_activities)))]        
+        print cumulative_social_grade
+
         
         return render_to_response('students/Marks_Report.html',Context({'student_data':student_data ,
         'mark_data':mark_data ,
         'attendance_data':attendance_data ,
         'co_curricular_data':co_curricular_data,
+        'cumulative_cocur_grade':cumulative_cocur_grade,                                                       
         'abhivyakti_vikas_data':abhivyakti_vikas_data ,
+        'cumulative_abhi_grade': cumulative_abhi_grade,
         'project_data':project_data ,
+        'cumulative_project_grade':cumulative_project_grade,
         'elocution_data':elocution_data,
+        'cumulative_elocution_grade': cumulative_elocution_grade,
         'physical_fit_info_data':physical_fit_info_data,
-        'social_activity_data':social_activity_data}))
+        'cumulative_physical_grade':cumulative_physical_grade,                                                         
+        'social_activity_data':social_activity_data,
+        'cumulative_social_grade':cumulative_social_grade,
+        'photo':photo}))
     else:
         return HttpResponse ('<html><body>Enter Registration Number<form action="" method="POST"><input type="text" name="reg_no" value="" id="reg_no" size="20"></td>	<input type="submit" value="Enter" /></form></body></html>')
 
