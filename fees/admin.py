c38a7c10560024d831b0874e627ceb80a4666004
Shantanoo <shantanoo@gmail.com>
2009-05-02 18:02:48 +0530

    added search field in fees tables

--- fees/admin.py
+++ fees/admin.py
@@ -6,16 +6,18 @@ from jp_sms.fees.models import FeeType, FeeReceipt
 import datetime
 
 class feetypeAdmin(admin.ModelAdmin):
-	list_display = ('ClassMaster','Type','Amount')
-	ordering = ('ClassMaster',)
-	list_filter = ['ClassMaster','Type']
-	pass
+    list_display = ('ClassMaster','Type','Amount')
+    ordering = ('ClassMaster',)
+    list_filter = ['ClassMaster','Type']
+    search_fields = ['ClassMaster__AcademicYear__Year', 'ClassMaster__Standard', 'ClassMaster__Division', 'ClassMaster__Teacher__Name' ]
+    pass
 				
 class feereceiptAdmin(admin.ModelAdmin):
-	list_display = ('ReceiptNumber','Date','StudentYearlyInformation','FeeType','Amount')
-	ordering = ('ReceiptNumber',)
-	list_filter = ['FeeType','StudentYearlyInformation']
-	pass
+    list_display = ('ReceiptNumber','Date','StudentYearlyInformation','FeeType','Amount')
+    ordering = ('ReceiptNumber',)
+    list_filter = ['FeeType','StudentYearlyInformation']
+    search_fields = ['ReceiptNumber', 'StudentYearlyInformation__StudentBasicInfo__FirstName', 'StudentYearlyInformation__StudentBasicInfo__RegistrationNo']
+    pass
 
 admin.site.register(FeeType, feetypeAdmin)
 admin.site.register(FeeReceipt, feereceiptAdmin)
