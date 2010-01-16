f3a401c8d0a589f9dff2f774fbc70e3cc491ac66
Shantanoo Mahajan <shantanoo@gmail.com>
2010-01-16 15:47:17 +0530

    Modified mapping. Every session will have classrom assigned to it. Hallticket generation logic changed.

--- pravesh/views.py
+++ pravesh/views.py
@@ -7,7 +7,11 @@ from django.contrib.auth.decorators import login_required
 from jp_sms.pravesh.models import ApplicationForm, GenerateHallTicketForm
 from jp_sms.pravesh.models import HallTicket, Session, ClassRoom, Student
 # Create your views here.
-
+#TBR
+import httplib
+import datetime
+import urllib
+#
 MEDIUM = {'E':'English', 'M':'Marathi'}
         
 def add(request):
@@ -20,7 +24,10 @@ def add(request):
             return render_to_response('pravesh/add.html', {'form': applicationform})
         if request.POST['PayMode'] == 'DD' and not request.POST['DDNo']:
             return render_to_response('pravesh/add.html', {'form': applicationform, 'message':'DD Number is mandatory'})
-        student_obj = Student()
+        if request.POST.has_key('edit'):
+            student_obj = Student(pk=request.POST['edit'])
+        else:
+            student_obj = Student()
         student_obj.FirstName = request.POST['FirstName']
         student_obj.MiddleName = request.POST['MiddleName']
         student_obj.LastName = request.POST['LastName']
@@ -41,43 +48,44 @@ def add(request):
         student_obj.PayMode = request.POST['PayMode']
         student_obj.DDNo = request.POST['DDNo']
         student_obj.save()
-        hallticket_obj = HallTicket()
-        hallticket_obj.Student = student_obj
-        hallticket_obj.Session = get_session(medium=student_obj.Medium)
-        hallticket_obj.ClassRoom = get_classroom(session_obj=hallticket_obj.Session, medium=student_obj.Medium)
-        hallticket_obj.SeatNumber = get_seatnumber()
-        print "HALLTICKET", hallticket_obj
-        hallticket_obj.save()
-        return redirect('/pravesh/hallticket/%s'%(hallticket_obj.SeatNumber))
-
+        x = get_seatnumber(medium=student_obj.Medium)
+        if not request.POST.has_key('edit'):
+            hallticket_obj = HallTicket()
+            hallticket_obj.Student = student_obj
+            hallticket_obj.ClassRoom = ClassRoom.objects.get(pk=x['classroom'])
+            hallticket_obj.SeatNumber = x['seatnumber']
+            print "HALLTICKET", hallticket_obj
+            hallticket_obj.save()
+            return redirect('/pravesh/hallticket/%s'%(hallticket_obj.pk))
+        return redirect('/pravesh')
 #
-def edit(request, seatnumber):
+def edit(request, pk):
     try:
-        hallticket_obj = HallTicket.objects.get(SeatNumber=seatnumber)
+        student_obj = Student.objects.get(pk=pk)
         data = {}
-        data['FirstName'] = hallticket_obj.Student.FirstName
-        data['MiddleName'] = hallticket_obj.Student.MiddleName
-        data['LastName'] = hallticket_obj.Student.LastName
-        data['FatherName'] = hallticket_obj.Student.FatherName
-        data['MotherName'] = hallticket_obj.Student.MotherName
-        data['Address'] = hallticket_obj.Student.Address
-        data['Pincode'] = hallticket_obj.Student.Pincode
-        data['PhoneHome'] = hallticket_obj.Student.PhoneHome
-        data['PhoneMobile'] = hallticket_obj.Student.PhoneMobile
-        data['Email'] = hallticket_obj.Student.Email
-        data['Medium'] = hallticket_obj.Student.Medium
-        data['Gender'] = hallticket_obj.Student.Gender
-        data['DateOfBirth'] = hallticket_obj.Student.DateOfBirth
-        data['CurrentSchool'] = hallticket_obj.Student.CurrentSchool
-        data['CurrentStd'] = hallticket_obj.Student.CurrentStd
-        data['PayMode'] = hallticket_obj.Student.PayMode
-        data['DDNo'] = hallticket_obj.Student.DDNo
+        data['FirstName'] = student_obj.FirstName
+        data['MiddleName'] = student_obj.MiddleName
+        data['LastName'] = student_obj.LastName
+        data['FatherName'] = student_obj.FatherName
+        data['MotherName'] = student_obj.MotherName
+        data['Address'] = student_obj.Address
+        data['Pincode'] = student_obj.Pincode
+        data['PhoneHome'] = student_obj.PhoneHome
+        data['PhoneMobile'] = student_obj.PhoneMobile
+        data['Email'] = student_obj.Email
+        data['Medium'] = student_obj.Medium
+        data['Gender'] = student_obj.Gender
+        data['DateOfBirth'] = student_obj.DateOfBirth
+        data['CurrentSchool'] = student_obj.CurrentSchool
+        data['CurrentStd'] = student_obj.CurrentStd
+        data['PayMode'] = student_obj.PayMode
+        data['DDNo'] = student_obj.DDNo
         applicationform = ApplicationForm(initial=data)
-        return render_to_response('pravesh/add.html', {'form':applicationform, 'button_name':'Update'})
+        return render_to_response('pravesh/add.html', {'form':applicationform, 'button_name':'Update', 'edit':student_obj.pk})
     except:
         return render_to_response('pravesh/add.html', {'message':'Record not found'})
     try:
-        hallticket_obj = HallTicket.objects.filter(SeatNumber=seatnumber)
+        hallticket_obj = HallTicket.objects.filter(pk=pk)
         applicationform = ApplicationForm()
         applicationform.FirstName = hallticket_obj.Student.FirstName
         return render_to_response('pravesh/add.html', {'form':applicationform})
@@ -85,22 +93,22 @@ def edit(request, seatnumber):
         return redirect('/pravesh/')
 
 
-def display_hallticket(request, seatnumber):
-
+def display_hallticket(request, pk):
     try:
-        hallticket_obj = HallTicket.objects.get(SeatNumber=seatnumber)
+        hallticket_obj = HallTicket.objects.get(pk=pk)
         data = {}
         data['firstname'] = hallticket_obj.Student.FirstName
         data['middlename'] = hallticket_obj.Student.MiddleName
         data['lastname'] = hallticket_obj.Student.LastName
-        data['hall_id'] = ' %s-%s-%s' % (hallticket_obj.Session.Number, hallticket_obj.ClassRoom.Number, hallticket_obj.SeatNumber)
+        data['hall_id'] = '%s%s-%s' % (hallticket_obj.ClassRoom.Medium, hallticket_obj.ClassRoom.Number, hallticket_obj.SeatNumber)
+        data['classroom'] = hallticket_obj.ClassRoom.Name
         data['medium'] = MEDIUM[hallticket_obj.Student.Medium]
-        data['date'] = hallticket_obj.Session.Start.strftime('%d-%b-%Y')
-        data['session'] = hallticket_obj.Session.Name
-        data['session_time'] = '%s to %s' % (hallticket_obj.Session.Start.strftime('%H:%M'), hallticket_obj.Session.End.strftime('%H:%M'))
+        data['date'] = hallticket_obj.ClassRoom.Session.Start.strftime('%d-%b-%Y')
+        data['session'] = hallticket_obj.ClassRoom.Session.Name
+        data['session_time'] = '%s to %s' % (hallticket_obj.ClassRoom.Session.Start.strftime('%H:%M'), hallticket_obj.ClassRoom.Session.End.strftime('%H:%M'))
         return render_to_response('pravesh/hallticket.html', {'data':data})
     except:
-        return render_to_response('pravesh/hallticket.html', {'msg': 'Seat number [<i>%s</i>] does not exists' % (seatnumber)})
+        return render_to_response('pravesh/hallticket.html', {'msg': 'Seat number [<i>%s</i>] does not exists' % (pk)})
 
 def index(request):
     data = {}
@@ -134,41 +142,31 @@ def generate_hallticket(request):
         return render_to_response('pravesh/generate_hallticket.html', {'form':genform, 'hallticket_objs':hallticket_objs})
     return render_to_response('pravesh/generate_hallticket.html', {'form':genform})
 
-def get_seatnumber():
-    tmp = HallTicket.objects.aggregate(Max('SeatNumber'))
-    tmp = tmp['SeatNumber__max']
-    if tmp == None: tmp = 0
-    return tmp+1
-    tmp = HallTicket.objects.all()
-    maxval = 0
-    for x in tmp:
-        if maxval < int(x.SeatNumber):
-            maxval = int(x.SeatNumber)
-    return str(maxval+1)
 
-def get_classroom(session_obj=None, medium=None):
-    retval = {}
-    for classroom in session_obj.classrooms.all():
-        if classroom.Medium != medium:
-            continue
-        retval[classroom.Number] = {}
-        retval[classroom.Number]['Capacity'] = classroom.Capacity
-        retval[classroom.Number]['Current'] = len(HallTicket.objects.filter(ClassRoom__Number=classroom.Number, Session=session_obj))
-    tmp = retval.keys()
-    tmp.sort()
-    for k in tmp:
-        if retval[k]['Current'] < retval[k]['Capacity']:
-            return ClassRoom.objects.get(Number=k)
-    return None
-
-
-def get_session(medium=None):
-    session_objs = Session.objects.all()
+def get_seatnumber(medium=None):
+    session_objs = Session.objects.all().order_by('Number', 'pk')
+    data = {}
     for session_obj in session_objs:
-        if get_classroom(session_obj=session_obj, medium=medium):
-            return session_obj
-    return None
-    pass
+        data[session_obj.pk] =  {}
+        classroom_objs = ClassRoom.objects.filter(Session=session_obj, Medium=medium).order_by('Number','pk')
+        for classroom_obj in classroom_objs:
+            data[session_obj.pk][classroom_obj.pk] = {}
+            data[session_obj.pk][classroom_obj.pk]['Capacity'] = classroom_obj.Capacity
+            data[session_obj.pk][classroom_obj.pk]['Current'] = len(HallTicket.objects.filter(ClassRoom=classroom_obj))
+    retval = {}
+    sessions = data.keys()
+    sessions.sort()
+    for s in sessions:
+        #retval['session'] = s
+        classrooms = data[s].keys()
+        print data
+        classrooms.sort()
+        for c in classrooms:
+            retval['classroom'] = c
+            if data[s][c]['Current'] < data[s][c]['Capacity']:
+                retval['seatnumber'] = data[s][c]['Current']+1
+                return retval
+    raise Exception("ERROR")
 
 add = login_required(add)
 display_hallticket = login_required(display_hallticket)
