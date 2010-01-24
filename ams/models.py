4912db9ef1d5622a80e8f264f43c8efea2a7dc57
aditya251 <aditya251@gmail.com>
2010-01-24 16:47:19 +0530

    AMS: Display 'Forgot Checkout' on leave page. Update late marks calculations per month basis.

---
+++ ams/models.py
@@ -0,0 +1,209 @@
+from django.db import models
+from django import forms
+
+# Create your models here.
+DAY_CHOICES = (
+    (1, 'Monday'),
+    (2, 'Tuesday'),
+    (3, 'Wednesday'),
+    (4, 'Thursday'),
+    (5, 'Friday'),
+    (6, 'Saturday'),
+    (7, 'Sunday'),
+)
+
+MONTH_CHOICES = (
+    (1, 'January'),
+    (2, 'February'),
+    (3, 'March'),
+    (4, 'April'),
+    (5, 'May'),
+    (6, 'June'),
+    (7, 'July'),
+    (8, 'August'),
+    (9, 'September'),
+    (10, 'October'),
+    (11, 'November'),
+    (12, 'December'),
+)
+
+REMARK_CHOICES = (
+	('A', 'Absent'),
+	('P', 'Present'),
+	('L', 'Late'),
+	('E', 'EarlyOut'),
+	('H', 'HalfDay'),
+	('O', 'Leave'),
+	('C', 'Compensatory Off'),
+	('S', 'Holiday'),
+	('D', 'OnDuty'),
+	('F', 'Approved HalfDay'),
+)
+
+STATUS_CHOICES = (
+	('I', 'In'),
+	('O', 'Out'),
+)
+
+LEAVE_CHOICES = (
+	(1, 'Casual'),
+	(2, 'Sick'),
+	(3, 'Earned'),
+	(4, 'OnDuty'),
+	(5, 'Halfday(First)'),
+	(6, 'Halfday(Second)'),
+)
+
+LEAVESTATUS_CHOICES = (
+	(1, 'Pending'),
+	(2, 'Approve'),
+	(3, 'Deny'),
+	(4, 'Pending Deny'),
+)
+
+YEAR_CHOICES = (
+	(1, 'Current'),
+	(2, 'Other'),
+)
+
+FORGOT_CHECKOUT_CHOICES = (
+	(1, 'Update Attendance'),
+	(2, 'Done'),
+)
+class AcademicYear(models.Model):
+	Id = models.AutoField(primary_key=True)
+	Title = models.CharField(max_length=30)
+	StartDate = models.DateField()
+	EndDate = models.DateField();
+	Status = models.PositiveIntegerField(choices=YEAR_CHOICES)
+	def __unicode__(self):
+		return "%s" % (self.Title)
+	
+class Category(models.Model):
+	Id = models.PositiveIntegerField(primary_key=True)
+	Description = models.CharField(max_length=30)
+	def __unicode__(self):
+		return "%s" % (self.Description)
+
+class User(models.Model):
+	Barcode = models.PositiveIntegerField(primary_key=True)
+	Category = models.ForeignKey(Category)
+	Name = models.CharField(max_length=30)
+	def __unicode__(self):
+		return "%s" % (self.Name)
+	def __str__(self):
+		return "%s" % (self.Name)
+
+class UserStatus(models.Model):
+	Barcode = models.ForeignKey(User)
+	Status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+class UserJoiningDate(models.Model):
+	Barcode = models.ForeignKey(User)
+	JoiningDate = models.DateField();
+	
+class ForgotCheckout(models.Model):
+	Barcode = models.ForeignKey(User)
+	Date = models.DateField()
+	Status = models.PositiveIntegerField(choices=FORGOT_CHECKOUT_CHOICES)
+
+class TimeRules(models.Model):
+	Type = models.CharField(max_length=30,primary_key=True)
+	TimeIn = models.TimeField()
+	LateIn = models.TimeField()
+	HalfIn = models.TimeField()
+	HalfOut = models.TimeField()
+	EarlyOut = models.TimeField()
+	TimeOut = models.TimeField()
+	def __unicode__(self):
+		return "%s" % (self.Type)
+		
+class DayRules(models.Model):
+	Category = models.ForeignKey(Category)
+	Barcode = models.PositiveIntegerField(null=True, blank=True)
+	Day = models.PositiveIntegerField(choices=DAY_CHOICES, null=True, blank=True)
+	Date = models.DateField(null=True, blank=True)
+	Type = models.ForeignKey(TimeRules)
+	def __unicode__(self):
+		return "%s %s %s %s  -  %s" % (self.Category, self.Barcode, self.Day, self.Date, self.Type)
+
+class Attendance(models.Model):
+	Barcode = models.ForeignKey(User)
+	Date = models.DateField(null=True, blank=True)
+	Remark = models.CharField(max_length=1, choices=REMARK_CHOICES)
+	Year = models.ForeignKey(AcademicYear)
+	Comment = models.CharField(max_length=30, null=True, blank=True)
+	
+class LeaveAttendance(models.Model):
+	Barcode = models.ForeignKey(User)
+	Date = models.DateField(null=True, blank=True)
+	Remark = models.CharField(max_length=1, choices=REMARK_CHOICES)
+
+class TempAttendance(models.Model):
+	Barcode = models.ForeignKey(User)
+	Remark = models.CharField(max_length=1, choices=REMARK_CHOICES)
+
+class TimeRecords(models.Model):
+	Barcode = models.ForeignKey(User)
+	Type = models.CharField(max_length=1, choices=STATUS_CHOICES)
+	Date = models.DateField()
+	Time = models.TimeField()
+
+class Leaves(models.Model):
+	Barcode = models.ForeignKey(User)
+	ApplicationDate = models.DateField()
+	LeaveDate = models.DateField()
+	ApprovalDate = models.DateField(null=True, blank=True)
+	Type = models.PositiveIntegerField(choices=LEAVE_CHOICES)
+	Status = models.PositiveIntegerField(choices=LEAVESTATUS_CHOICES)
+	Reason = models.CharField(max_length=30, null=True, blank=True)
+	def __unicode__(self):
+		return "%s %s %s" % (self.Barcode, self.LeaveDate, self.Status)
+
+class LeavesBalance(models.Model):
+	Barcode = models.ForeignKey(User)
+	Type = models.PositiveIntegerField(choices=LEAVE_CHOICES)
+	Days = models.FloatField()
+
+class LeaveRules(models.Model):
+	Category = models.ForeignKey(Category)
+	Type = models.PositiveIntegerField(choices=LEAVE_CHOICES)
+	Days = models.PositiveIntegerField()
+
+class EncashLeaves(models.Model):
+	Barcode = models.ForeignKey(User)
+	Days = models.PositiveIntegerField()
+	Status = models.PositiveIntegerField(choices=LEAVESTATUS_CHOICES)
+	
+class Overtime(models.Model):
+	Barcode = models.ForeignKey(User)
+	Date = models.DateField()
+	ApprovalDate = models.DateField(null=True, blank=True)
+	Hours = models.FloatField()
+	Status = models.PositiveIntegerField(choices=LEAVESTATUS_CHOICES)
+
+class LeaveForm(forms.Form):
+	Category = forms.ModelChoiceField(queryset=Category.objects.all())
+	Barcode = forms.ModelChoiceField(queryset=User.objects.all())
+	FromDate =  forms.DateField(widget=forms.DateTimeInput, required=False)
+	ToDate =  forms.DateField(widget=forms.DateTimeInput, required=False)
+	Days = forms.IntegerField(required=False)
+	Type = forms.ChoiceField(choices=LEAVE_CHOICES)
+	Reason = forms.CharField(max_length=30, required=False)
+
+class ReportForm(forms.Form):
+	Category = forms.ModelChoiceField(queryset=Category.objects.all())
+	Barcode = forms.ModelChoiceField(queryset=User.objects.all())
+	FromDate =  forms.DateField(widget=forms.DateTimeInput)
+	ToDate =  forms.DateField(widget=forms.DateTimeInput)
+
+class DailyReportForm(forms.Form):
+	Category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
+	Date =  forms.DateField(widget=forms.DateTimeInput)
+
+class DayRulesForm(forms.Form):
+	FromDate =  forms.DateField(widget=forms.DateTimeInput)
+	ToDate =  forms.DateField(widget=forms.DateTimeInput)
+	Category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
+	Barcode = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
+	Type = forms.ModelChoiceField(queryset=TimeRules.objects.all(), required=False)
---
+++ urls.py
@@ -0,0 +1,38 @@
+from django.conf.urls.defaults import *
+#from students import views
+# The next two lines enable the admin and load each admin.py file:
+from django.views.generic.simple import direct_to_template
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/ams/home/', 'ams.views.admin_home'),
+    (r'^admin/(.*)', admin.site.root),
+    (r'^marks_add/', 'students.views.marks_add'),
+    (r'^attendance_add/', 'students.views.attendance_add'),
+    (r'^report/', 'students.views.report'),
+    (r'^ams/', 'ams.views.get_barcode'),
+    (r'^leave/', 'ams.views.app_leave'),
+    (r'^ams_report/', 'ams.views.monthly_report'),
+    (r'^ams_daily_report/', 'ams.views.daily_report'),
+    (r'^ams_display/', 'ams.views.ams_display'),
+    (r'^fee/', 'fees.views.fee_receipt'),
+    (r'^fee_receipt/', 'fees.views.reprint_receipt'),
+    (r'^fee_report/', 'fees.views.fee_report'),
+    (r'^reportPDF/', 'students.views.reportPDF'),
+    (r'^certificatePDF/', 'students.views.certificatePDF'),
+    (r'^schoolLeavingPDF/', 'students.views.schoolLeavingPDF'),
+    (r'^fee_collection/', 'fees.views.fee_collection'),
+    #(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^ams_dayrules/', 'ams.views.add_dayrules'),
+    ##(r'^accounts/', include('registration.backends.default.urls')),
+    (r'^accounts/', include('registration.urls')),
+    (r'^$', direct_to_template,  { 'template': 'index.html' }, 'index'),
+    (r'^pravesh/add', 'pravesh.views.add'),
+    (r'^pravesh/hallticket/(\d+)/?$','pravesh.views.display_hallticket'),
+    (r'^pravesh/student/(\d+)/edit/?$','pravesh.views.edit'),
+    (r'^pravesh/generate_hallticket','pravesh.views.generate_hallticket'),
+    (r'^pravesh/?','pravesh.views.index'),
+    #(r'^useradd/', views.user_add),
+    # ... the rest of your URLs here ...
+)
