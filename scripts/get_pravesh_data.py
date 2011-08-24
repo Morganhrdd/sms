6b425c23d7061db3795134bad7a62105d584e562
Shantanoo <shantanoo@gmail.com>
2011-08-24 20:21:38 +0530

    pep8 modifications

--- scripts/get_pravesh_data.py
+++ scripts/get_pravesh_data.py
@@ -6,14 +6,32 @@ wb = Workbook()
 wb = xlwt.Workbook()
 ws = wb.add_sheet('2011-12 Pravesh')
 
-cols = ['FirstName', 'MiddleName', 'LastName', 'FatherName', 'MotherName', 'Address', 'Pincode', 'PhoneHome', 'PhoneMobile', 'Email', 'Medium', 'Gender', 'DateOfBirth', 'CurrentSchool', 'CurrentStd', 'PayMode', 'DDNo']
+cols = [
+    'FirstName',
+    'MiddleName',
+    'LastName',
+    'FatherName',
+    'MotherName',
+    'Address',
+    'Pincode',
+    'PhoneHome',
+    'PhoneMobile',
+    'Email',
+    'Medium',
+    'Gender',
+    'DateOfBirth',
+    'CurrentSchool',
+    'CurrentStd',
+    'PayMode',
+    'DDNo'
+]
 for i in range(len(cols)):
-    ws.write(0,i, cols[i])
+    ws.write(0, i, cols[i])
 
 r = 1
 for x in s:
     for i in range(len(cols)):
-        ws.write(r, i, getattr(x,cols[i]))
+        ws.write(r, i, getattr(x, cols[i]))
     r += 1
 
 wb.save('pravesh_2011-2012.xls')
