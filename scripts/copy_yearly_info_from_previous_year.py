9dd9d3aacf303f34dae443133f9e7890ccd49b9a
Shantanoo <shantanoo@gmail.com>
2011-08-24 20:19:34 +0530

    pep8 modifications

--- scripts/copy_yearly_info_from_previous_year.py
+++ scripts/copy_yearly_info_from_previous_year.py
@@ -4,9 +4,12 @@ import sys
 
 prev_year = raw_input("Enter previous year: ")
 next_year = raw_input("Enter next year: ")
-for std in [5,6,7,8,9]:
+for std in [5, 6, 7, 8, 9]:
     try:
-        s = StudentYearlyInformation.objects.filter(ClassMaster__Standard=std, ClassMaster__AcademicYear__Year=prev_year)
+        s = StudentYearlyInformation.objects.filter(
+            ClassMaster__Standard=std,
+            ClassMaster__AcademicYear__Year=prev_year
+        )
     except Exception, e:
         print e
         sys.exit(1)
@@ -15,8 +18,11 @@ for std in [5,6,7,8,9]:
         n = StudentYearlyInformation()
         n.StudentBasicInfo = x.StudentBasicInfo
         n.RollNo = x.RollNo
-        n.ClassMaster = ClassMaster.objects.get(Standard=std+1, Division=x.ClassMaster.Division, AcademicYear__Year=next_year)
-        n.Hostel = '1' # No hostel set to all
+        n.ClassMaster = ClassMaster.objects.get(
+            Standard = std + 1,
+            Division = x.ClassMaster.Division,
+            AcademicYear__Year = next_year
+        )
+        n.Hostel = '1'  # No hostel set to all
         n.save()
         print n
-
